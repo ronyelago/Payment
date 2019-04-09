d0b12eb3bb8622d2653b7f46998296893c2ccddb
Ronye <ronye.rocha@gmail.com>
2019-04-09 17:38:22 -0300

    more props

--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -12,17 +12,20 @@ namespace PaymentContext.Domain.Entities
         public decimal TotalPayd { get; set; }
         public string Address { get; set; }
         public string Document { get; set; }
-
+        public string Email { get; set; }       
     }
 
     public class BoletoPayment : Payment
     {
-
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; }
     }
 
     public class CreditCardPayment : Payment
     {
-
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string LastTransactionNumber { get; set; }
     }
 
     public class PayPalPayment : Payment
