9873eb0a565f98045194dcdd0ed91b469f7e83ba
Ronye <ronye.rocha@gmail.com>
2019-04-09 17:31:37 -0300

    more props

--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -4,11 +4,15 @@ namespace PaymentContext.Domain.Entities
 {
     public abstract class Payment
     {
+        public string Payer { get; set; }
         public string Number { get; set; }
         public DateTime PaidDate { get; set; }   
         public DateTime ExpireDate { get; set; }
         public decimal Total { get; set; }
         public decimal TotalPayd { get; set; }
+        public string Address { get; set; }
+        public string Document { get; set; }
+
     }
 
     public class BoletoPayment : Payment
--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -6,5 +6,6 @@ namespace PaymentContext.Domain.Entities
         public string LastName { get; set; }
         public string Document { get; set; }
         public string Email { get; set; }
+        public string Address { get; set; }
     }
 }
