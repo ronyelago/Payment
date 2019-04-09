785353435d029d72a38fef9a2f57ab4de3dfcbde
Ronye <ronye.rocha@gmail.com>
2019-04-09 19:12:30 -0300

    subscription opent/close

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -1,14 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Subscription
     {
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
-        public DateTime? ExpireDate { get; set; }
+        public Subscription(DateTime? expireDate)
+        {
+            ExpireDate = expireDate;
+            CreateDate = DateTime.Now;
+            LastUpdateDate = DateTime.Now;
+            Active = true;
+            
+            _payments = new List<Payment>();
+        }
+
+        private IList<Payment> _payments;
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
         public bool Active { get; set; }
-        public List<Payment> Payments { get; set; }
+        public IReadOnlyCollection<Payment> Payments { get { return _payments.ToArray(); } }
+
+        public void AddPayment(Payment payment)
+        {
+            _payments.Add(payment);
+        }
     }
 }
