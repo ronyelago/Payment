e984c4c369a4c60cde0525c9cdeb69ba4cb14e95
Ronye <ronye.rocha@gmail.com>
2019-04-10 01:45:17 -0300

    address.cs

---
+++ PaymentContext.Domain/ValueObjects/Email.cs
@@ -0,0 +1,12 @@
+﻿namespace paymentcontext.domain.ValueObjects
+{
+    public class Email
+    {
+        public Email(string address)
+        {
+            Address = address;
+        }
+
+        public string Address { get; private set; }
+    }
+}
