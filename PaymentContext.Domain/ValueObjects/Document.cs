a950c456584cee662575263a73fb4a5dfc60fcfa
Ronye <ronye.rocha@gmail.com>
2019-04-11 01:37:38 -0300

    more validations

--- PaymentContext.Domain/ValueObjects/Document.cs
+++ PaymentContext.Domain/ValueObjects/Document.cs
@@ -1,4 +1,5 @@
-﻿using paymentcontext.domain.Enums;
+﻿using Flunt.Validations;
+using paymentcontext.domain.Enums;
 using paymentcontext.shared.ValueObjects;
 
 namespace paymentcontext.domain.ValueObjects
@@ -9,9 +10,29 @@ namespace paymentcontext.domain.ValueObjects
         {
             Number = number;
             Type = type;
+
+            AddNotifications(new Contract()
+                    .Requires()
+                    .IsTrue(Validate(), "Document.Number", "Documento inválido")
+                );
         }
 
         public string Number { get; private set; }
         public EDocumentType Type { get; private set; }
+
+        public bool Validate()
+        {
+            if (Type == EDocumentType.CNPJ && Number.Length == 14)
+            {
+                return true;
+            }
+
+            if (Type == EDocumentType.CPF && Number.Length == 11)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
