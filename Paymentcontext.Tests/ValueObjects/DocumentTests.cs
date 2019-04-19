c3413db29aa131fa17eba4adde5f53e173015608
Ronye <ronye.rocha@gmail.com>
2019-04-19 11:58:56 -0300

    DocumentTests

---
+++ Paymentcontext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using paymentcontext.domain.Enums;
+using paymentcontext.domain.ValueObjects;
+
+namespace paymentcontext.tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        // Red, Green, Refactor
+        [TestMethod]
+        public void ShoudReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("1234", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessIfCNPJIsValid()
+        {
+
+        }
+
+        public void ShoudReturnErrorWhenCPFIsInvalid()
+        {
+
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessIfCPFIsValid()
+        {
+
+        }
+    }
+}
