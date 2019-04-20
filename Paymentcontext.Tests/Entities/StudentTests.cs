a78594670cf3ecd9cbd7e5e1a1b378b1d7920823
Ronye <ronye.rocha@gmail.com>
2019-04-20 12:00:48 -0300

    new Student

--- Paymentcontext.Tests/Entities/StudentTests.cs
+++ Paymentcontext.Tests/Entities/StudentTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using paymentcontext.domain.Enums;
+using paymentcontext.domain.ValueObjects;
 using PaymentContext.Domain.Entities;
 
 namespace PaymentContext.Tests.Entities
@@ -6,10 +8,20 @@ namespace PaymentContext.Tests.Entities
     [TestClass]
     public class StudentTests
     {
-        //[TestMethod]
-        public void TestMethod1()
+        [TestMethod]
+        public void ShoudReturnErrorWhenHadActiveSubscription()
         {
-            var subscription = new Subscription(null);
+            
+        }
+
+        [TestMethod]
+        public void ShoudReturnErrorWhenHadNoActiveSubscription()
+        {
+            var name = new Name("Bruce", "Wayne");
+            var document = new Document("37810823899", EDocumentType.CPF);
+            var email = new Email("bruce.wayne@gmail.com");
+
+            var student = new Student(name, document, email);
         }
     }
 }
