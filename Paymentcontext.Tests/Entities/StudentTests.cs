6a4998e1e49439241845e6c4bfaa6eea335d54f3
Ronye <ronye.rocha@gmail.com>
2019-04-21 15:06:36 -0300

    refactor

--- Paymentcontext.Tests/Entities/StudentTests.cs
+++ Paymentcontext.Tests/Entities/StudentTests.cs
@@ -9,34 +9,38 @@ namespace PaymentContext.Tests.Entities
     [TestClass]
     public class StudentTests
     {
+        private readonly Name _name;
+        private readonly Document _document;
+        private readonly Address _address;
+        private readonly Email _email;
         private readonly Student _student;
         private readonly Subscription _subscription;
 
         public StudentTests()
         {
-            var name = new Name("Bruce", "Wayne");
-            var document = new Document("37810823899", EDocumentType.CPF);
-            var email = new Email("bruce.wayne@gmail.com");
-            var address = new Address("Rua Wash", "122", "Cool Bairro", "Gotham", "SP", "BR", "06795005");
+            _name = new Name("Bruce", "Wayne");
+            _document = new Document("37810823899", EDocumentType.CPF);
+            _email = new Email("bruce.wayne@gmail.com");
+            _address = new Address("Rua Wash", "122", "Cool Bairro", "Gotham", "SP", "BR", "06795005");
 
-            _student = new Student(name, document, email);
+            _student = new Student(_name, _document, _email);
             _subscription = new Subscription(null);
-
-            var payment = new PayPalPayment("123456", "Wayne Corp", "4321", DateTime.Now, DateTime.Now, 10, 10, address, document, email);
-
-            _subscription.AddPayment(payment);
         }
 
         [TestMethod]
         public void ShoudReturnErrorWhenHadActiveSubscription()
         {
-            
+            var payment = new PayPalPayment("123456", "Wayne Corp", "4321", DateTime.Now, DateTime.Now, 10, 10, _address, _document, _email);
+            _subscription.AddPayment(payment);
+            _student.AddSubscription(_subscription);
+            _student.AddSubscription(_subscription);
+
+            Assert.IsTrue(_student.Invalid);
         }
 
         [TestMethod]
-        public void ShoudReturnErrorWhenHadNoActiveSubscription()
+        public void ShoudReturnSuccessWhenHadActiveSubscription()
         {
-            _student.AddSubscription(_subscription);
             _student.AddSubscription(_subscription);
 
             Assert.IsTrue(_student.Invalid);
@@ -45,11 +49,9 @@ namespace PaymentContext.Tests.Entities
         [TestMethod]
         public void ShoudReturnErrorWhenSubscriptionHasNoPayment()
         {
-            var name = new Name("Bruce", "Wayne");
-            var document = new Document("37810823899", EDocumentType.CPF);
-            var email = new Email("bruce.wayne@gmail.com");
-
-            var student = new Student(name, document, email);
+            _student.AddSubscription(_subscription);
+            
+            Assert.IsTrue(_student.Invalid);
         }
     }
 }
