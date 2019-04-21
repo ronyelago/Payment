9e314400ec3839ce1abf01d2da04a18a30565089
Ronye <ronye.rocha@gmail.com>
2019-04-21 15:30:25 -0300

    AreEquals

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -42,7 +42,7 @@ namespace PaymentContext.Domain.Entities
             AddNotifications(new Contract()
                 .Requires()
                 .IsFalse(hasActiveSubscription, "Student.Subscriptions", "Você já possui uma assinatura ativa")
-                .IsGreaterThan(0, subscription.Payments.Count, "Student.Subscription.Payments", "Esta assinatura não possui pagamentos."));
+                .AreEquals(0, subscription.Payments.Count, "Student.Subscription.Payments", "Esta assinatura não possui pagamentos."));
         }
     }
 }
